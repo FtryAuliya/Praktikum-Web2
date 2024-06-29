cf85469fbe7fcf1b20b03ed50ce54506aab41bcf
Fitriya Auliya <168781076+FtryAuliya@users.noreply.github.com>
2024-06-29 20:52:06 +0800

    Initial commit

---
+++ profile.php
@@ -0,0 +1,53 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/Auliya.jpg" alt="Fitriya Auliya" width="150" height="170" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NIK / NPM :</strong></td>
+                        <td align="left" width="50%">13 0520 205 / 2210010620</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Fitriya Auliya</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 13 Mei 2000</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0838 1722 4314</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">triyauliya@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="" target="_blank" style="text-decoration:none;">
+                                <img src="img/facebook.png" width="30" alt="facebook">
+                            </a>
+                            <a href="https://www.instagram.com/ftryauliya" target="_blank" style="text-decoration:none;">
+                                <img src="img/instagram.png" width="30" alt="instagram">
+                            </a>
+                            <a href="https://x.com/ftryauliya" target="_blank" style="text-decoration:none;">
+                                <img src="img/twitter.png" width="30" alt="twitter">
+                            </a>
+                            <a href="" target="_blank">
+                                <img src="img/linkedin.png" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
